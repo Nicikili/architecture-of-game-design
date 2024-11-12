7c829911d09512859f9f11c7cf94e0af2225658f
Nicikili <127138055+Nicikili@users.noreply.github.com>
2024-11-12 18:31:09 +0100

    Nici's scene Updates
    add playzone

---
+++ Mythical Berliner Escalation/Assets/_Nici/Code/Scripts/ChildScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ChildScript : ParentScript
+{
+private protected override void Start()
+    { 
+
+    }
+
+private protected override void Update()
+
+    {
+
+    }
+}
---
+++ Mythical Berliner Escalation/Assets/_Nici/Code/Scripts/ParentScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ParentScript : MonoBehaviour
+{
+    private protected virtual void Start()
+    {
+
+    }
+
+    private protected virtual void Update()
+
+    {
+
+    }
+}
